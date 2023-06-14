ce3d7f7d13ee8591d6874f05006ddd0db5d7554a
Edvan Ribeiro <jk.jr47@gmail.com>
2023-06-14 10:45:11 -0300

    Criação do componente Header

---
+++ desafios/coffee_delivery/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { Logo, HeaderContainer } from './style'
+import logoImg from '../../assets/Logo.svg'
+import { Cart } from '../Cart'
+import { Location } from '../Location'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <nav>
+        <Logo>
+          <img src={logoImg} alt="Logo da cafeteria Coffee Delivery" />
+        </Logo>
+        <div>
+          <Location />
+          <Cart />
+        </div>
+      </nav>
+    </HeaderContainer>
+  )
+}
---
+++ desafios/coffee_delivery/src/components/Header/style.ts
@@ -0,0 +1,23 @@
+import { styled } from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  padding: 2rem 2rem 0;
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  nav > div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 0.75rem;
+  }
+`
+
+export const Logo = styled.a`
+  display: flex;
+`
