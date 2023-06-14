3af39e7bbe2a48130a9595ed828f124bb38b88cc
Edvan Ribeiro <jk.jr47@gmail.com>
2023-06-14 10:44:32 -0300

    Definido estilos globais e thema padrão

---
+++ desafios/coffee_delivery/src/styles/global.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+
+:root {
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+
+    line-height: 1.3;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing:border-box;
+}
+
+body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+}
+
+body {
+    display: flex;
+    justify-content:center;
+    align-items:center;
+    background-color: ${(props) => props.theme.color.background};
+}
+
+#root {
+   width: 70rem;
+}
+
+`
---
+++ desafios/coffee_delivery/src/styles/themes/default.ts
@@ -0,0 +1,100 @@
+export const defaultTheme = {
+  color: {
+    background: '#FAFAFA',
+    white: '#FFFFFF',
+
+    yellow: '#DBAC2C',
+    'yellow-light': '#F1E9C9',
+    'yellow-dark': '#C47F17',
+
+    purple: '#8047F8',
+    'purple-light': '#EBE5F9',
+    'purple-dark': '#4B2995',
+
+    'base-title': '#272221',
+    'base-subtitle': '#403937',
+    'base-text': '#574F4D',
+    'base-label': '#8D8686',
+    'base-hover': '#D7D5D5',
+
+    'base-button': '#E6E5E5',
+    'base-input': '#EDEDED',
+    'base-card': '#F3F2F2',
+  },
+  font: {
+    TitleXL: {
+      size: '3rem',
+      LineHeight: 1.3,
+      Weight: 800,
+    },
+
+    TitleL: {
+      size: '2rem',
+      LineHeight: 1.3,
+      Weight: 800,
+    },
+    TitleM: {
+      size: '1.5rem',
+      LineHeight: 1.3,
+      Weight: 800,
+    },
+    TitleS: {
+      size: '1.25rem',
+      LineHeight: 1.3,
+      Weight: 700,
+    },
+    TitleXS: {
+      size: '1.125rem',
+      LineHeight: 1.3,
+      Weight: 700,
+    },
+
+    TextLBold: {
+      size: '1.25rem',
+      LineHeight: 1.3,
+      Weight: 700,
+    },
+    TextMBold: {
+      size: '1rem',
+      LineHeight: 1.3,
+      Weight: 700,
+    },
+
+    TextLRegular: {
+      size: '1.25rem',
+      LineHeight: 1.3,
+      Weight: 400,
+    },
+    TextMRegular: {
+      size: '1rem',
+      LineHeight: 1.3,
+      Weight: 400,
+    },
+    TextS: {
+      size: '0.875rem',
+      LineHeight: 1.3,
+      Weight: 400,
+    },
+    TextXS: {
+      size: '0.75rem',
+      LineHeight: 1.3,
+      Weight: 700,
+    },
+
+    Tag: {
+      size: '0.625rem',
+      LineHeight: 1.3,
+      Weight: 700,
+    },
+    ButtonG: {
+      size: '0.875rem',
+      LineHeight: 1.6,
+      Weight: 700,
+    },
+    ButtonM: {
+      size: '0.75rem',
+      LineHeight: 1.6,
+      Weight: 400,
+    },
+  },
+}
